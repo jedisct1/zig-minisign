46cbbff6639ebbf350d6e3a5092023dd707f3a3d
Frank Denis <github@pureftpd.org>
2024-10-08 14:35:06 +0200

    Update clap

--- src/clap.zig
+++ src/clap.zig
@@ -13,10 +13,13 @@ const testing = std.testing;
 pub const args = @import("clap/args.zig");
 pub const parsers = @import("clap/parsers.zig");
 pub const streaming = @import("clap/streaming.zig");
+pub const ccw = @import("clap/codepoint_counting_writer.zig");
 
 test "clap" {
     testing.refAllDecls(@This());
 }
+
+pub const default_assignment_separators = "=";
 
 /// The names a `Param` can have.
 pub const Names = struct {
@@ -643,6 +646,7 @@ test "Diagnostic.report" {
 pub const ParseOptions = struct {
     allocator: mem.Allocator,
     diagnostic: ?*Diagnostic = null,
+    assignment_separators: []const u8 = default_assignment_separators,
 };
 
 /// Same as `parseEx` but uses the `args.OsIterator` by default.
@@ -662,6 +666,7 @@ pub fn parse(
         // Let's reuse the arena from the `OSIterator` since we already have it.
         .allocator = arena.allocator(),
         .diagnostic = opt.diagnostic,
+        .assignment_separators = opt.assignment_separators,
     });
 
     return Result(Id, params, value_parsers){
@@ -733,6 +738,7 @@ pub fn parseEx(
         .params = params,
         .iter = iter,
         .diagnostic = opt.diagnostic,
+        .assignment_separators = opt.assignment_separators,
     };
     while (try stream.next()) |arg| {
         // TODO: We cannot use `try` inside the inline for because of a compiler bug that
@@ -761,7 +767,7 @@ pub fn parseEx(
     // fields to slices and return that.
     var result_args = Arguments(Id, params, value_parsers, .slice){};
     inline for (meta.fields(@TypeOf(arguments))) |field| {
-        if (@typeInfo(field.type) == .Struct and
+        if (@typeInfo(field.type) == .@"struct" and
             @hasDecl(field.type, "toOwnedSlice"))
         {
             const slice = try @field(arguments, field.name).toOwnedSlice(allocator);
@@ -878,7 +884,7 @@ fn deinitArgs(
         // If the multi value field is a struct, we know it is a list and should be deinited.
         // Otherwise, it is a slice that should be freed.
         switch (@typeInfo(@TypeOf(field))) {
-            .Struct => @field(arguments, longest.name).deinit(allocator),
+            .@"struct" => @field(arguments, longest.name).deinit(allocator),
             else => allocator.free(@field(arguments, longest.name)),
         }
     }
@@ -931,7 +937,7 @@ fn Arguments(
         i += 1;
     }
 
-    return @Type(.{ .Struct = .{
+    return @Type(.{ .@"struct" = .{
         .layout = .auto,
         .fields = &fields,
         .decls = &.{},
@@ -953,6 +959,24 @@ test "str and u64" {
         .allocator = testing.allocator,
     });
     defer res.deinit();
+}
+
+test "different assignment separators" {
+    const params = comptime parseParamsComptime(
+        \\-a, --aa <usize>...
+        \\
+    );
+
+    var iter = args.SliceIterator{
+        .args = &.{ "-a=0", "--aa=1", "-a:2", "--aa:3" },
+    };
+    var res = try parseEx(Help, &params, parsers.default, &iter, .{
+        .allocator = testing.allocator,
+        .assignment_separators = "=:",
+    });
+    defer res.deinit();
+
+    try testing.expectEqualSlices(usize, &.{ 0, 1, 2, 3 }, res.args.aa);
 }
 
 test "everything" {
@@ -1130,10 +1154,10 @@ pub fn help(
     const max_spacing = blk: {
         var res: usize = 0;
         for (params) |param| {
-            var cs = io.countingWriter(io.null_writer);
+            var cs = ccw.codepointCountingWriter(io.null_writer);
             try printParam(cs.writer(), Id, param);
-            if (res < cs.bytes_written)
-                res = @intCast(cs.bytes_written);
+            if (res < cs.codepoints_written)
+                res = @intCast(cs.codepoints_written);
         }
 
         break :blk res;
@@ -1143,22 +1167,22 @@ pub fn help(
         opt.description_indent +
         max_spacing * @intFromBool(!opt.description_on_new_line);
 
-    var first_paramter: bool = true;
+    var first_parameter: bool = true;
     for (params) |param| {
-        if (!first_paramter)
+        if (!first_parameter)
             try writer.writeByteNTimes('\n', opt.spacing_between_parameters);
 
-        first_paramter = false;
+        first_parameter = false;
         try writer.writeByteNTimes(' ', opt.indent);
 
-        var cw = io.countingWriter(writer);
+        var cw = ccw.codepointCountingWriter(writer);
         try printParam(cw.writer(), Id, param);
 
         const Writer = DescriptionWriter(@TypeOf(writer));
         var description_writer = Writer{
             .underlying_writer = writer,
             .indentation = description_indentation,
-            .printed_chars = @intCast(cw.bytes_written),
+            .printed_chars = @intCast(cw.codepoints_written),
             .max_width = opt.max_width,
         };
 
@@ -1237,8 +1261,7 @@ pub fn help(
             } else {
                 // For none markdown like format, we just respect the newlines in the input
                 // string and output them as is.
-                var i: usize = 0;
-                while (i < non_emitted_newlines) : (i += 1)
+                for (0..non_emitted_newlines) |_|
                     try description_writer.newline();
             }
 
@@ -1269,7 +1292,7 @@ fn DescriptionWriter(comptime UnderlyingWriter: type) type {
             debug.assert(word.len != 0);
 
             var first_word = writer.printed_chars <= writer.indentation;
-            const chars_to_write = word.len + @intFromBool(!first_word);
+            const chars_to_write = try std.unicode.utf8CountCodepoints(word) + @intFromBool(!first_word);
             if (chars_to_write + writer.printed_chars > writer.max_width) {
                 // If the word does not fit on this line, then we insert a new line and print
                 // it on that line. The only exception to this is if this was the first word.
@@ -1721,6 +1744,50 @@ test "clap.help" {
         \\-d, --dd <V3>...    Both repeated option.
         \\
     );
+
+    // Test with multibyte characters.
+    try testHelp(.{
+        .indent = 0,
+        .max_width = 46,
+        .description_on_new_line = false,
+        .description_indent = 4,
+        .spacing_between_parameters = 2,
+    },
+        \\-a                  Shört flåg.
+        \\
+        \\
+        \\-b <V1>             Shört öptiön.
+        \\
+        \\
+        \\    --aa            Löng fläg.
+        \\
+        \\
+        \\    --bb <V2>       Löng öptiön.
+        \\
+        \\
+        \\-c, --cc            Bóth fläg.
+        \\
+        \\
+        \\    --complicate    Fläg wíth ä cömplǐcätéd
+        \\                    änd vërý löng dèscrıptıön
+        \\                    thät späns mültíplë
+        \\                    lınēs.
+        \\
+        \\                    Pärägräph number 2:
+        \\                    * Bullet pöint
+        \\                    * Bullet pöint
+        \\
+        \\                    Exämple:
+        \\                        sömething sömething
+        \\                        sömething
+        \\
+        \\
+        \\-d, --dd <V3>       Böth öptiön.
+        \\
+        \\
+        \\-d, --dd <V3>...    Böth repeäted öptiön.
+        \\
+    );
 }
 
 /// Will print a usage message in the following format:
@@ -1729,18 +1796,18 @@ test "clap.help" {
 /// First all none value taking parameters, which have a short name are printed, then non
 /// positional parameters and finally the positional.
 pub fn usage(stream: anytype, comptime Id: type, params: []const Param(Id)) !void {
-    var cos = io.countingWriter(stream);
+    var cos = ccw.codepointCountingWriter(stream);
     const cs = cos.writer();
     for (params) |param| {
         const name = param.names.short orelse continue;
         if (param.takes_value != .none)
             continue;
 
-        if (cos.bytes_written == 0)
+        if (cos.codepoints_written == 0)
             try stream.writeAll("[-");
         try cs.writeByte(name);
     }
-    if (cos.bytes_written != 0)
+    if (cos.codepoints_written != 0)
         try cs.writeAll("]");
 
     var has_positionals: bool = false;
@@ -1759,7 +1826,7 @@ pub fn usage(stream: anytype, comptime Id: type, params: []const Param(Id)) !voi
             continue;
         };
 
-        if (cos.bytes_written != 0)
+        if (cos.codepoints_written != 0)
             try cs.writeAll(" ");
 
         try cs.writeAll("[");
@@ -1783,7 +1850,7 @@ pub fn usage(stream: anytype, comptime Id: type, params: []const Param(Id)) !voi
         if (param.names.short != null or param.names.long != null)
             continue;
 
-        if (cos.bytes_written != 0)
+        if (cos.codepoints_written != 0)
             try cs.writeAll(" ");
 
         try cs.writeAll("<");
