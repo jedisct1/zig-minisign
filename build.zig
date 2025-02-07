9743302d012529448c20fc0ec74f6f4bdffc9849
Frank Denis <github@pureftpd.org>
2025-02-07 23:08:03 +0100

    Remove unneeded indent block

--- build.zig
+++ build.zig
@@ -19,26 +19,24 @@ pub fn build(b: *std.Build) void {
     });
 
     // Build minzign cli
-    {
-        const exe = b.addExecutable(.{
-            .name = "minizign",
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        });
+    const exe = b.addExecutable(.{
+        .name = "minizign",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
 
-        exe.root_module.addImport("minizign", minizign_module);
+    exe.root_module.addImport("minizign", minizign_module);
 
-        b.installArtifact(exe);
+    b.installArtifact(exe);
 
-        const run_cmd = b.addRunArtifact(exe);
-        run_cmd.step.dependOn(b.getInstallStep());
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
 
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
-        }
-
-        const run_step = b.step("run", "Run the app");
-        run_step.dependOn(&run_cmd.step);
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
     }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
 }
