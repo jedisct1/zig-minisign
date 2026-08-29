6afbab9dbe666f0dcd2f281d48fcea69a976e99d
Tobias Simetsreiter <dasimmet@gmail.com>
2026-08-29 21:46:14 +0200

    compat for 0.16.0 and 0.17.0 build args

--- build.zig
+++ build.zig
@@ -55,8 +55,12 @@ pub fn build(b: *std.Build) void {
         const run_cmd = b.addRunArtifact(exe);
         run_cmd.step.dependOn(b.getInstallStep());
 
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
+        if (@hasDecl(@TypeOf(run_cmd.*), "addPassthruArgs")) {
+            run_cmd.addPassthruArgs();
+        } else if (@hasField(std.Build, "args")) {
+            if (b.args) |args| {
+                run_cmd.addArgs(args);
+            }
         }
 
         const run_step = b.step("run", "Run the app");
